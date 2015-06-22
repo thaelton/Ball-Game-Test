147d647a9492407ff65b1dffee0b26ab18de7e5c
Lucy <Lucy@Candys-Mac-mini-5.local>
2015-06-22 11:03:49 -0400

    Test

---
+++ roll a ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	private int count;
+	public float speed;
+
+	public Text countText;
+	public Text winText;
+
+	void Start (){
+
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		winText.text = "";
+		SetCountText ();
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement*speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		}
+
+	}
+
+	void SetCountText (){
+		countText.text = "Score: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You are awesome!";
+		}
+	}
+}
---
+++ roll a ball/Assets/Scripts/WallsController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallsController : MonoBehaviour {
+
+	public Color wallColor;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
